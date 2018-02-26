230d813eb6570a36faf9c54de602938c1d57a1ee
Kamoseal <serna.karla@gmail.com>
2018-02-26 20:44:01 +0000

    Sin validar fecha

---
+++ js/README.md
@@ -0,0 +1,74 @@
+Valida datos de tarjetas de crédito
+Track: Common Core
+Curso: JS Deep Dive: Crea tu propia librería usando JavaScript
+Unidad: Producto final
+La función debe recibir un elemento DOM que contenga <input>s con los siguientes nombres (atributo name):
+
+cn (Card Number): El número de la tarjeta de crédito
+exp (Expiry Date): Fecha de expiración
+cvv (Card Verification Value): Código de validación de 3 dígitos
+name: Nombre completo como aparece en la tarjeta
+Ejemplo
+<form>
+  <div class="form-group">
+    <label for="cn">Número de tarjeta</label>
+    <input id="cn" name="cn" />
+  </div>
+  <div class="form-group">
+    <label for="exp">Fecha de vencimiento</label>
+    <input id="exp" name="exp" />
+  </div>
+  <div class="form-group">
+    <label for="cvv">CVV</label>
+    <input id="cvv" name="cvv" />
+  </div>
+  <div class="form-group">
+    <label for="name">Nombre completo</label>
+    <input id="name" name="name" />
+  </div>
+  <input type="submit" value="Pagar" />
+</form>
+const form = document.querySelector("form");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  if (validateCardDetails(form)) {
+    console.log("datos válido... enviar...");
+  } else {
+    console.log("datos inválidos");
+  }
+});
+
+function validateCardDetails(element) {
+  //escribe tu código aqui
+}
+A la hora de hacer la validacion, tu funcion debería de añadir la clase .error al <input> que no pasen la validación, o la clase .success en caso de que sí pase.
+
+Usar el algoritmo de Luhn, el cual únicamente usa los numeros de la tarjeta de crédito. No usa el código de verificacion, fecha de vencimiento , el nombre, ni la dirección.
+
+Cosas a considerar:
+Necesitas usar métodos de arreglo (.forEach, .map,etc.) sin embargo, estos metodos son para arreglos. Si yo hago:
+const form = document.querySelector("form");
+¿Tengo un arreglo? ¿Algo diferente? ¿Cómo le hago para implementar metodos de arreglo en otras cosas que no son arreglos?
+
+La solucion se tiene que hacer con ES6 con los temas vistos en clase.
+Validaciones por realizar
+Fecha vencimiento (solo 3 validaciones son necesarias)
+No string vacio
+solo numeros
+que sean 2 digitos de año y 2 de mes
+los numeros no pueden ser negativos
+el mes no puede ser mayor a 12
+Que el numero sea futuro (no hoy ni antes)
+Revisar el formato
+CVV (todas son necesarias)
+No string vacio
+Sean SOLO 3 digitos (no string ni boolean ni nada)
+los numeros sean positivos (que numeros son positivos de 3 digitos¿)
+Nombre (3 de las 6 son necesarias)
+No string vacio
+SOLO Sea string
+Cada palabra maximo tiene 30 caracteres
+mayusculas y minusculas
+MINIMO 2 palabras
+No empieza con espacios
---
+++ js/app.js
@@ -0,0 +1,98 @@
+//Funcion pintar color rojo el dato no ingresado BOOTSTRAP
+(function(validate) {
+    'use strict';
+    window.addEventListener('load', function() {
+      let forms = document.getElementsByClassName('needs-validation');
+      let validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
+
+const form = document.querySelector('form');
+const arrayOfElements = Array.from(form);
+let cn = arrayOfElements.find(cn => cn.id === "cn");
+let name = arrayOfElements.find(name => name.id === "name");
+let cD = arrayOfElements.find(cD => cD.id === "cD");
+
+console.log(cD)
+cn.addEventListener('keyup', e =>{
+    let cnValue = cn.value;
+    let cnValueInArray = cnValue
+    .toString()
+    .split("")
+    .reverse()
+    .map(number => parseInt(number))
+    console.log(cnValueInArray);
+    let multiplyOddCN = cnValueInArray.map((number, index)=>{
+        if (index % 2 == 1){
+            return number * 2;
+        }else{
+            return number
+        }
+    });
+    console.log(multiplyOddCN); 
+    let sumOddifLengthTen = multiplyOddCN.map(number =>{
+        if (number >=10){
+            return number - 9;
+        }else{
+            return number
+        }
+    })
+    console.log(sumOddifLengthTen); 
+    let sumAllNumbers= sumOddifLengthTen.reduce((a,b)=>{
+        return a + b;
+    })
+    console.log(sumAllNumbers);
+    
+    if (sumAllNumbers % 10 === 0){
+        return cn.className ="was-validated form-control";
+    }else{
+        return cn.className = "noValide form-control";
+    }
+});
+
+name.addEventListener('keyup', e =>{
+    let nameValue = name.value;
+    console.log(nameValue)
+    function validateName (string){
+        let delateSpacesNameValueFirstEnd = string.trim();
+        let arrayOfWords = delateSpacesInNameValueFirstEnd.split(" ");
+        if (delateSpacesInNameValueFirstEnd.length >=2 ){
+            return name.classList.add =("was-validated form-control");
+        }else{
+            return name.classList.add = ("noValide form-control");
+        }
+    }
+});
+cD.addEventListener('keyup', e =>{
+    let cDValue = cD.value;
+    function validatecD (number){
+        if (number > 000 ){
+            return cD.classList.add =("was-validated");
+        }else {
+            return cD.classList.add = ("noValide");
+        }
+    }
+});
+
+  
+
+//let cD = arrayOfElements.find(cD => cD.id === "cD");
+//let cnValueNumber = parseInt(cn.value);
+  //  function valueCn(cnValue){
+        
+  //      console.log(cnValueNumber)
+  //  }
+
+//const inputs = form.getElementsByTagName('input');
+//const selects = form.getElementsByTagName('select');
+//const cn = inputs[0];
+//const month = selects[0];
+//const
